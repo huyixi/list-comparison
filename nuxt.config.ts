b3edea83350d4f884d853dc09c7cd01a20c3d392
huyixi <hi@huyixi.com>
2025-07-17 16:38:10 +0800

    Remove unused CSS file from Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
-  css: ["~/assets/css/main.css", "~/assets/css/ocr.css"],
+  css: ["~/assets/css/main.css"],
   icon: {
     customCollections: [{ prefix: "custom", dir: "./assets/icons" }],
   },
