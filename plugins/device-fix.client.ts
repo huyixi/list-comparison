7696c08e33becd07f78696cdda17471e5ba1a359
huyixi <hi@huyixi.com>
2025-07-27 10:33:21 +0800

    chore

---
+++ plugins/device-fix.client.ts
@@ -0,0 +1,35 @@
+export default defineNuxtPlugin({
+  name: "device-fix",
+  enforce: "post",
+  setup() {
+    const device = useDevice();
+    const ipadTabletFix = useCookie("ipadTabletFix", {
+      maxAge: 60 * 60 * 24 * 365,
+    });
+    if (import.meta.server) {
+      if (ipadTabletFix?.value === "true") {
+        device.isTablet = true;
+        device.isMobileOrTablet = true;
+        device.isDesktop = false;
+      }
+    }
+
+    if (import.meta.client) {
+      if (
+        !device.isTablet &&
+        !device.isMobile &&
+        device.isApple &&
+        window.ontouchstart !== undefined
+      ) {
+        device.isTablet = true;
+        device.isMobileOrTablet = true;
+        device.isDesktop = false;
+
+        if (ipadTabletFix.value !== "true") {
+          ipadTabletFix.value = "true";
+          reloadNuxtApp({ force: true });
+        }
+      }
+    }
+  },
+});
